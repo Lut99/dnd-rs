b73d0bc8f9b790c8ca3ff12eb09faa0d7e993a9a
Lut99 <mullertim99@gmail.com>
2024-04-06 15:17:47 +0200

    Initial commit
    Already included small project structure and some expected dependencies.

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+//  MAIN.rs
+//    by Lut99
+//
+//  Created:
+//    06 Apr 2024, 15:12:56
+//  Last edited:
+//    06 Apr 2024, 15:13:57
+//  Auto updated?
+//    Yes
+//
+//  Description:
+//!   Entrypoint to the DnD server binary.
+//
+
+
+/***** LIBRARY *****/
+fn main() {}
